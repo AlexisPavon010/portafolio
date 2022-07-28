76468e3f4e27ee44d7ca122644baa57b04218dc6
Alexis Pavon <themaster034@gmail.com>
2022-07-28 13:24:26 -0300

    add download link cv

--- src/components/Contacts.tsx
+++ src/components/Contacts.tsx
@@ -1,4 +1,4 @@
-const CV_URL = 'https://export-download.canva.com/9vepk/DAEKlv9vepk/200/0/0001-31701518471.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAJHKNGJLC2J7OGJ6Q%2F20220728%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20220728T063207Z&X-Amz-Expires=38150&X-Amz-Signature=673752168fb28b8a3d422a31bcf50bd898662031ba859f19112a2c4f0f8aa198&X-Amz-SignedHeaders=host&response-content-disposition=attachment%3B%20filename%2A%3DUTF-8%27%27Alexis%2520Pavon.png&response-expires=Thu%2C%2028%20Jul%202022%2017%3A07%3A57%20GMT'
+const CV_URL = 'https://export-download.canva.com/9vepk/DAEKlv9vepk/200/0-31702953977.pdf?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAJHKNGJLC2J7OGJ6Q%2F20220728%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20220728T003607Z&X-Amz-Expires=63184&X-Amz-Signature=e8a95935128caa4bfc4d09ffbdf95c1546c8ffe3d6fbca018d7e53bd2e927104&X-Amz-SignedHeaders=host&response-content-disposition=attachment%3B%20filename%2A%3DUTF-8%27%27Alexis%2520Pavon.pdf&response-expires=Thu%2C%2028%20Jul%202022%2018%3A09%3A11%20GMT'
 
 
 export const Contacts = () => {
