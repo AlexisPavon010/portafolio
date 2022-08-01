71d3560be571000d64803425ab510a761fa07a76
Alexis Pavon <themaster034@gmail.com>
2022-08-01 14:58:18 +0200

    fix url cv

--- src/components/Contacts.tsx
+++ src/components/Contacts.tsx
@@ -1,4 +1,4 @@
-const CV_URL = 'https://export-download.canva.com/9vepk/DAEKlv9vepk/200/0-31702953977.pdf?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAJHKNGJLC2J7OGJ6Q%2F20220728%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20220728T003607Z&X-Amz-Expires=63184&X-Amz-Signature=e8a95935128caa4bfc4d09ffbdf95c1546c8ffe3d6fbca018d7e53bd2e927104&X-Amz-SignedHeaders=host&response-content-disposition=attachment%3B%20filename%2A%3DUTF-8%27%27Alexis%2520Pavon.pdf&response-expires=Thu%2C%2028%20Jul%202022%2018%3A09%3A11%20GMT'
+const CV_URL = 'https://www.canva.com/design/DAEKlv9vepk/5O5dai-_IYcdm9qAIsSmOA/view?utm_content=DAEKlv9vepk&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink'
 
 
 export const Contacts = () => {
